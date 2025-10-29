300b255ea3896e88bd21a9006125578e969b334d
mondal_saikat <104445016+saikatkumarmondal@users.noreply.github.com>
2025-10-29 16:28:28 +0600

    add dist into client folder

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const distPath = path.join(__dirname, "client", "dist");
 app.use(express.static(distPath));
 
 // ✅ SPA fallback (works with new path-to-regexp)
-app.get("/*(.*)", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(distPath, "index.html"));
 });
 
